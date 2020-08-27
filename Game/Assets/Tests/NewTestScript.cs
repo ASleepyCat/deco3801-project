0eb818be83ac8b6ca44a8d1419cb8371c4ea0a02
ASleepyCat <dyeom340@gmail.com>
2020-08-27 17:52:03 +1000

    Add passing tests

--- Game/Assets/Tests/NewTestScript.cs
+++ Game/Assets/Tests/NewTestScript.cs
@@ -13,7 +13,7 @@ namespace Tests
         public void NewTestScriptSimplePasses()
         {
             // Use the Assert class to test conditions
-            Assert.True(false);
+            Assert.True(true);
         }
 
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use

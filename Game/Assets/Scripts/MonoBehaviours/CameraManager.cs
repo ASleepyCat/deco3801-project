e5df0d322de1c83ad2d949c554474321a58d17f5
jaydenj <s4435178@student.uq.edu.au>
2020-10-04 20:45:23 +1000

    Added Camera Bounds

--- Game/Assets/Scripts/MonoBehaviours/CameraManager.cs
+++ Game/Assets/Scripts/MonoBehaviours/CameraManager.cs
@@ -5,13 +5,17 @@ namespace MonoBehaviours
     public class CameraManager : MonoBehaviour
     {
         public Transform target;
-
+        public Vector2 minPosition;
+        public Vector2 maxPosition;
+        
         // Update is called once per frame
         private void Update()
         {
             var transform1 = transform;
-            var position = target.transform.position;
-            transform1.position = new Vector3(position.x, position.y, transform1.position.z);
+            Vector3 targetPosition = new Vector3(target.transform.position.x, target.transform.position.y, transform1.position.z);
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
+            transform1.position = new Vector3(targetPosition.x, targetPosition.y, transform1.position.z);
         }
     }
 }

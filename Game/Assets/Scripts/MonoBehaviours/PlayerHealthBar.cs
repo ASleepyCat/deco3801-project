908af508be11361da285de404a25fed17cabba47
ASleepyCat <dyeom340@gmail.com>
2020-10-06 13:33:57 +1000

    Implement health system and UI

---
+++ Game/Assets/Scripts/MonoBehaviours/PlayerHealthBar.cs
@@ -0,0 +1,27 @@
+﻿using ScriptableObjects;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MonoBehaviours
+{
+    public class PlayerHealthBar : MonoBehaviour
+    {
+        public Transform healthParent;
+        
+        private PlayerHealth _health;
+        private Image[] _points;
+
+        private void Start()
+        {
+            _health = ScriptableObject.CreateInstance<PlayerHealth>();
+            _health.onHealthDecrementCallback = UpdateHealth;
+            _points = healthParent.GetComponentsInChildren<Image>();
+        }
+
+        private void UpdateHealth()
+        {
+            _points[_health.health].sprite = null;
+            _points[_health.health].enabled = false;
+        }
+    }
+}

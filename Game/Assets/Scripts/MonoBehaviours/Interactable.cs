aa6e0f0d5b7d45fe8855dfa68ba922d84b236301
ASleepyCat <dyeom340@gmail.com>
2020-10-29 11:31:53 +1000

    Add scroll item

--- Game/Assets/Scripts/MonoBehaviours/Interactable.cs
+++ Game/Assets/Scripts/MonoBehaviours/Interactable.cs
@@ -6,7 +6,10 @@ namespace MonoBehaviours
     public class Interactable : MonoBehaviour
     {
         //Stored current VA when inside a trigger
+        public BoxCollider2D trigger;
+        public Item item;
         public VIDE_Assign inTrigger;
+
         private GameManager _gameManager;
 
         private void Start()
@@ -36,6 +39,11 @@ namespace MonoBehaviours
    
             if (!_gameManager.PlayerState.SetPlayerState(PlayerState.States.InDialogue)) return;
             UIManager2.Instance.Interact(inTrigger);
+            if (item != null)
+            {
+                GameManager.Instance.inventory.AddItem(item);
+                trigger.isTrigger = false;
+            }
             //_gameManager.PlayerState.SetPlayerState(PlayerState.States.Free);
         }
     }
